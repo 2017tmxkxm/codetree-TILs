2ce7a180885e8082aff5ae03cd60c3c290fbdea9
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2024-09-03 15:48:10 +0900

    Title: 비교에 따른 연산, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240903/비교에 따른 연산/operation-based-on-comparison.java
+++ 240903/비교에 따른 연산/operation-based-on-comparison.java
@@ -6,7 +6,7 @@ public class Main {
         int b = sc.nextInt();
 
         if(a>b) System.out.println(a*b);
-        else System.out.println(b%a);
+        else System.out.println(b/a);
         
     }
 }
