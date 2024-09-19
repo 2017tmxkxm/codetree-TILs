5a91bba43b21e21e09113b0baa678e418ce64110
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2024-09-19 21:39:09 +0900

    Title: 알파벳 출력하기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240919/알파벳 출력하기/print-alphabet.java
+++ 240919/알파벳 출력하기/print-alphabet.java
@@ -9,7 +9,9 @@ public class Main {
         for(int i=0; i<n; i++) {
             for(int j=0; j<i+1; j++) {
                 System.out.print(temp);
+                if(temp == 90) temp = 'A';
                 temp++;
+
             }
             System.out.println();
         }
