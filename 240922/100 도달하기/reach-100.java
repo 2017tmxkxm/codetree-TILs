63563f43c3d60b53cca4e736a0599375dfb56516
2017tmxkxm <142385212+2017tmxkxm@users.noreply.github.com>
2024-09-22 12:59:23 +0900

    Title: 100 도달하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240922/100 도달하기/reach-100.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[10];
+
+        int cnt = 0;
+
+        arr[0] = 1;
+        arr[1] = n;
+
+        for(int i=2; i<100; i++) {
+            arr[i] = arr[i-2] + arr[i-1];
+            if(arr[i] > 100) {
+                cnt = i;
+                break;
+            }
+        }
+
+        for(int i=0; i<=cnt; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
